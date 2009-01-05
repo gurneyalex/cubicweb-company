bb7cbd12393348c297f902537a328f65cecaf8d9
Nicolas Chauvat <nicolas.chauvat@logilab.fr>
2009-01-05 22:36:08 +0100

    allow for longer company names

--- schema.py
+++ schema.py
@@ -3,7 +3,7 @@ from cubicweb.schema import format_constraint
 # template's specific schema
 
 class Division(EntityType):
-    name = String(required=True, fulltextindexed=True, indexed=True, maxsize=64)
+    name = String(required=True, fulltextindexed=True, indexed=True, maxsize=128)
     headquarters = SubjectRelation('PostalAddress', cardinality='*?', composite='subject')
     web = String(fulltextindexed=True, maxsize=128)
     phone = SubjectRelation('PhoneNumber', cardinality='*?', composite='subject')
