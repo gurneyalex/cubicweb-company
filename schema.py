08dd611b1b3546d0768409769f08bfb34fba29ee
sylvain thenault <sylvain.thenault@logilab.fr>
2009-05-13 11:10:43 +0200

    fix schema def

--- schema.py
+++ schema.py
@@ -1,14 +1,15 @@
 # template's specific schema
-
-class Division(EntityType):
+class _Base(EntityType):
     name = String(required=True, fulltextindexed=True, indexed=True, maxsize=128)
     headquarters = SubjectRelation('PostalAddress', cardinality='*?', composite='subject')
     web = String(fulltextindexed=True, maxsize=128)
     phone = SubjectRelation('PhoneNumber', cardinality='*?', composite='subject')
     use_email = SubjectRelation('EmailAddress', cardinality='*+', composite='subject')
+
+class Division(_Base):
     is_part_of = SubjectRelation('Company', cardinality='1*')
 
-class Company(Division):
+class Company(_Base):
     rncs = String(fulltextindexed=True, maxsize=32)
     subsidiary_of = SubjectRelation('Company', cardinality='?*')
 
