d7fbd2cf7495b30202db9362fb050628cfbcff5d
Nicolas Chauvat <nicolas.chauvat@logilab.fr>
2009-09-22 12:15:33 +0200

    B fix links for see also box

--- views/boxes.py
+++ views/boxes.py
@@ -7,6 +7,20 @@ from cubicweb.web.htmlwidgets import SideBoxWidget, BoxLink
 def has_rncs(entity):
     return entity.rncs is not None
 
+def url_societecom(entity):
+    url = 'http://www.societe.com/'
+    if entity.rncs:
+        url += 'cgi-bin/recherche?rncs=%s' % entity.rncs
+    return url
+
+def url_score3(entity):
+    url = 'http://www.score3.fr/'
+    if entity.rncs:
+        url += 'entreprises.shtml?chaine=%s' % entity.rncs
+    elif entity.name:
+        url += 'entreprises.shtml?chaine=%s' % entity.name
+    return url
+
 class CompanySeeAlso(EntityBoxTemplate):
     id = 'company_seealso_box'
     __select__ = EntityBoxTemplate.__select__ & implements('Company') #& score_entity(has_rncs)
@@ -14,9 +28,8 @@ class CompanySeeAlso(EntityBoxTemplate):
 
     def cell_call(self, row, col, **kwargs):
         entity = self.entity(row, col)
-        rncs = entity.rncs or u''
         box = SideBoxWidget(self.req._('This company on other sites'),
                             'company_sites%i' % entity.eid)
-        box.append(BoxLink('http://www.societe.com/cgi-bin/recherche?rncs=%s'% rncs, u'Société.com'))
-        box.append(BoxLink('http://www.score3.fr/entreprises.shtml?chaine=%s' % rncs, u'Score3.fr'))
+        box.append(BoxLink(url_societecom(entity), u'Société.com'))
+        box.append(BoxLink(url_score3(entity), u'Score3.fr'))
         self.w(box.render())
