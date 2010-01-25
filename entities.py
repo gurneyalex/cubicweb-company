1555e77cba51ec35b8f948b98ddd50e7bac929b9
Sylvain Th?nault <sylvain.thenault@logilab.fr>
2010-01-25 20:08:12 +0100

    cw 3.6 api update

--- entities.py
+++ entities.py
@@ -1,13 +1,13 @@
 """this contains the template-specific entities' classes
 
 :organization: Logilab
-:copyright: 2003-2007 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
+:copyright: 2003-2010 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
 :contact: http://www.logilab.fr/ -- mailto:contact@logilab.fr
 """
 __docformat__ = "restructuredtext en"
 
 from cubicweb.entities import AnyEntity, fetch_config
-from cubicweb.common.mixins import TreeMixIn
+from cubicweb.mixins import TreeMixIn
 from cubicweb.interfaces import ITree
 
 class Division(AnyEntity):
