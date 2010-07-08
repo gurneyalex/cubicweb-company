fe1b140e0d3de964f2d9ef49e102a87da160cd09
Sylvain Th?nault <sylvain.thenault@logilab.fr>
2010-07-08 18:50:06 +0200

    fix setup.py

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -6,14 +6,11 @@ distname = 'cubicweb-%s' % modname
 
 numversion = (0, 5, 0)
 version = '.'.join(str(num) for num in numversion)
-
 license = 'LGPL'
-
 author = 'Logilab'
 author_email = 'contact@logilab.fr'
 web = 'http://www.cubicweb.org/project/%s' % distname
 description = 'company component for the CubicWeb framework'
-
 classifiers = [
     'Environment :: Web Environment',
     'Framework :: CubicWeb',
